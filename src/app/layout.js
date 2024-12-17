4af1499f9c52e8e7fccce205112ba8db2fbedd0c
AbhiAbhiAbhii <abhilash@gradical.xyz>
2024-12-17 18:13:00 +0530

    implemented script

--- src/app/layout.js
+++ src/app/layout.js
@@ -34,6 +34,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
+      <script src="https://analytics.ahrefs.com/analytics.js" data-key="ut5KAFc0XMCmv3kF3RxTug" defer="true"></script>
         <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
